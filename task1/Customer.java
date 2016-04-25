b2255d924574fb9c7f733dccad63caa84a91fde5
Igor Santa Cruz <Igor Santa Cruz@ISANTACRUM-DH02.jalasoft.local>
2016-04-25 14:47:09 -0400

    Task1 Refactor

--- task1/Customer.java
+++ task1/Customer.java
@@ -25,30 +25,11 @@ class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double thisAmount = each.calculateAmount();
             // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+            frequentRenterPoints = frequentRenterPoints + each.calculateCurrentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(thisAmount) + "\n";
--- task1/Rental.java
+++ task1/Rental.java
@@ -16,4 +16,33 @@ class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    public double calculateAmount(){
+        double amount = 0;
+        switch (_movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (_daysRented > 2)
+                    amount += (_daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += _daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (_daysRented > 3)
+                    amount += (_daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public int calculateCurrentRenterPoints() {
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((_movie.getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                _daysRented > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
