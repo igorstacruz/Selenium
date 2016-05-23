7a211f38150fe6d8229a93706fa192e04a2b176a
Igor Santa Cruz <igor.stacruz@gmail.com>
2016-05-23 17:17:54 -0400

    Task 2 Lead Form

---
+++ task2/src/main/java/com/jalasoft/testing/pages/MainContainer.java
@@ -0,0 +1,19 @@
+package com.jalasoft.testing.pages;
+
+/**
+ * Created by Igor Santa Cruz on 5/18/2016.
+ */
+public class MainContainer extends AbstractBasePage {
+
+    public Header goToHeader() {
+        return new Header();
+    }
+
+    public TabBar goToTabBar() {
+        return new TabBar();
+    }
+
+    public Footer goToFooter() {
+        return new Footer();
+    }
+}
