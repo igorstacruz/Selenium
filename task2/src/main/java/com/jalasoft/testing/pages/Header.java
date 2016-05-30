92732e8e8ead5d537056c8ce753deede20ae516d
Igor Santa Cruz <igor.stacruz@gmail.com>
2016-05-30 16:30:40 -0400

    remove wrong files for task2

--- task2/src/main/java/com/jalasoft/testing/pages/Header.java
+++
@@ -1,7 +0,0 @@
-package com.jalasoft.testing.pages;
-
-/**
- * Created by Igor Santa Cruz on 5/18/2016.
- */
-public class Header {
-}
--- task2/src/test/java/com/jalasoft/testing/LeadsTest.java
+++
@@ -1,7 +0,0 @@
-package com.jalasoft.testing;
-
-/**
- * Created by Igor Santa Cruz on 5/18/2016.
- */
-public class LeadsTest {
-}
